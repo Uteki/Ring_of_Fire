5e0e7c1336c214faf8441da49640e13c2656652f
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-08-25 23:06:17 +0200

    Deck and more

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 
+import { Game } from '../../models/game';
+
 @Component({
   selector: 'app-game',
   standalone: true,
@@ -13,10 +15,20 @@ import {NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game | undefined;
 
   constructor() {
+  }
+
+  ngOnInit():void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
   }
 
   takeCard() {
---
+++ src/models/game.ts
@@ -0,0 +1,25 @@
+export class Game {
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCard: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('spade_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+    }
+
+    this.shuffle(this.stack);
+  }
+
+  shuffle(array: string[]): void {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+}
